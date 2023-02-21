559effbc4dd220efc31bce9da3f27276303452cf
AttackSwan <124640611+AttackSwan@users.noreply.github.com>
2023-02-21 15:05:49 +1100

    Create html and js. Write pseudocode

---
+++ javascript.js
@@ -0,0 +1,18 @@
+function game(){
+    //loop for 5 rounds
+        //call playARound()
+        //debug: console.log() winner of round and results
+    //count winner/loser totals
+}
+
+function getComputerChoice(){
+    //get computer choice
+    //debug: console.log(choice)
+    //return choice
+}
+
+function playARound(playerSelection, computerSelection){
+    //compare inputs (ensure case-insensitive)
+    //debug: console.log()
+    //return winner in the form of a string - e.g. "You lose! Paper beats Rock"
+}
